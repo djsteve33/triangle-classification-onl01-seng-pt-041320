6978ae8e265e25353cc0772309ac08be3075e175
Steve Womble <swomble@aol.com>
2020-05-21 20:31:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,8 @@ class Triangle
    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
      raise TriangleError
    elseif (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) ||
-     (side_2)
+     (@side_2 + @side_3 <= @side_1)
+     raise TriangleError
    
  end
 end
